0a587e42022a01b9368a6fa86867b04c079c3172
stevenli <steven4354@gmail.com>
2019-04-10 11:20:19 -0700

    namespace explanation

--- c_fundamentals/main.cpp
+++ c_fundamentals/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+// what a namespace is
+// https://www.geeksforgeeks.org/difference-namespace-class/
+
+// optional: using below allows you to shorten and everytime cout is called it is always from iostream (std name)
+// it is generally safer to use ::
+// using namespace std
+
 int main() {
-	std::cout << "Welcome to c fundamentals \n";
+	std::cout << "Hello to c fundamentals \n";
 	return 0;
 }
