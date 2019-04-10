0790a4598a44dd0c3f384474e49b8a51da19a0fd
stevenli <steven4354@gmail.com>
2019-04-10 11:33:11 -0700

    using variables in logs

--- c_fundamentals/main.cpp
+++ c_fundamentals/main.cpp
@@ -5,9 +5,14 @@
 
 // optional: using below allows you to shorten and everytime cout is called it is always from iostream (std name)
 // it is generally safer to use ::
-// using namespace std
+// syntax: using namespace std
 
 int main() {
+
+	constexpr int WORLD_LENGTH = 9;
+
 	std::cout << "Hello to c fundamentals \n";
+	std::cout << "This is how to place variables in stirngs " << WORLD_LENGTH;
+
 	return 0;
 }
