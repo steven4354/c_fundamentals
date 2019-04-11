54ff89698170f6befc487495812a6a619383ce91
stevenli <steven4354@gmail.com>
2019-04-10 19:37:56 -0700

    using functions in c++

--- c_fundamentals/main.cpp
+++ c_fundamentals/main.cpp
@@ -8,24 +8,32 @@
 // it is generally safer to use ::
 // syntax: using namespace std
 
-int main() {
 
+// creating a function - use void at the beginning if it doesn't return anything!
+void PrintIntro() {
 	constexpr int WORLD_LENGTH = 9;
 
 	std::cout << "Hello to c fundamentals \n";
 	std::cout << "This is how to place variables in strings: " << WORLD_LENGTH;
 	std::cout << "\n";
+}
+
+int main() {
+
+	// calling a function in c++
+	PrintIntro();
 
 	// below is how to get an input from the terminal / console
 	// string comes from the string library
 
-	std::cout << "Place an input \n ";
+	std::cout << "Give an input:   ";
 	std::string UserInput = "";
 	std::cin >> UserInput;
-	std::cout << "\n ";
+	std::cout << "\n";
 
 	// repeat the input
 	std::cout << "This was your input " << UserInput;
+	std::cout << "\n";
 
 	return 0;
 }
